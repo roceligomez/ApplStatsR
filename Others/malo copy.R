efefed15431abf78669950f248f767c7452dc456
Francisco <lf.rosalesm@up.edu.pe>
2018-04-23 12:07:13 -0500

    some more files

--- Others/malo copy.R
+++
@@ -1,4 +0,0 @@
-fWLM<-function(y,X_mat,w){T0<-t(X_mat)%*%diag(w)%*%X_mat
-t<-system.time({t_1<-solve(T0)%*%t(X_mat)%*%(w*y);t2<-X_mat%*%t_1})
-return(list(beta=t_1,hat=t2,stddev=sqrt(sum(w*(t2-y)^2))/
-(length(y)-ncol(X_mat)), wts=w,t=t[[3]]))}
