8ba869fa3fcc7c1a9192ff7b022e255c6b9c2b5b
emely09 <38511453+emely09@users.noreply.github.com>
2018-05-06 18:38:58 -0500

    Add files via upload

--- Homework/GRAFICOS_EMELYCONDOR.R
+++ Homework/GRAFICOS_EMELYCONDOR.R
@@ -1,4 +1,5 @@
-
+#ADVANCEDGRAPHIPS
+#EXERCISE 1
 dat <- read.csv2( "https://github.com/franciscorosales-marticorena/ApplStatsR/raw/master/Exercises/school_math.raw",sep = " ", stringsAsFactors = TRUE)
 dat
 #a
@@ -17,13 +18,25 @@ linearmodel <- lm(data = dat, mathach~ ses)
 abline(linearmodel, lty = "dashed", lwd = 2)
 
 hist(dat$mathach)
+
 hist(dat$ses, freq = FALSE)
 lines(density(dat$ses), lty = 3, col = "blue", lwd = 2)
+
 boxplot(mathach ~ school, data = dat, col.axis = "red", 
         pch = 15 , lty = "dashed")
 title("Boxplot")
 
-
+#EXERCISE 2
+matricita <- matrix(c(1,1,1,
+                2,2,2,
+                3,3,3),3,3)
+layout(matricita, width = c(.5,1,1))
+layout.show(3)
+boxplot(dat$mathach,axes = FALSE)
+boxplot(dat$ses, axes =FALSE)
+plot(dat$mathach, dat$ses)
+lineamodel <- lm(data = dat, ses ~ mathach)
+abline(lineamodel, lty = "dashed", lwd = 2)
 
 
 
